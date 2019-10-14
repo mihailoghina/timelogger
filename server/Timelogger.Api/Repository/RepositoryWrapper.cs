e2a8cbd52078ce905605669e8c9a4cb24a212a5b
Mihai <mihailoghina@gmail.com>
2019-10-14 23:23:32 +0300

    fix warnings

--- server/Timelogger.Api/Repository/RepositoryWrapper.cs
+++ server/Timelogger.Api/Repository/RepositoryWrapper.cs
@@ -9,9 +9,9 @@ namespace Timelogger.Api.Repository
     public class RepositoryWrapper : IRepositoryWrapper
     {
         private ApiContext _apiContext;
-        private IUsersRepository _userRepository;
-        private IProjectsRepository _projectRepository;
-        private IActivityRepository _activityRepository;
+        private IUsersRepository _userRepository = null;
+        private IProjectsRepository _projectRepository = null;
+        private IActivityRepository _activityRepository = null;
         private ILogger _logger;
 
         public RepositoryWrapper(ApiContext apiContext, ILogger logger)
