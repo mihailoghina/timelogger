6ccb1edc49a311ec4d88b61aec86e91ca8b8d46b
Mihai <mihailoghina@gmail.com>
2019-10-17 00:32:54 +0300

    added required attribute to Project entities

--- server/Timelogger/Entities/Project/ProjectUpdateDTO.cs
+++ server/Timelogger/Entities/Project/ProjectUpdateDTO.cs
@@ -8,7 +8,9 @@ namespace Timelogger.Entities
         [Required]
 		[MinLength(5, ErrorMessage="Project name must have at least 5 characters")]
 		public string Name { get; set; }
+		[Required]
 		public bool IsComplete { get; set; }
+		[Required]
 		public DateTime DeadLineDate { get; set; }
 	}
 }
